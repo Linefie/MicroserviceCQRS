0ae9672f64c1604a94f08a47d83cadba1d39e50b
Line Jensen <lysline85@hotmail.com>
2020-10-15 11:53:54 +0200

    Add project files.

---
+++ CQRS.WebApi/Features/ProductFeatures/Queries/GetAllProductsQuery.cs
@@ -0,0 +1,40 @@
+﻿using CQRS.WebApi.Context;
+using CQRS.WebApi.Models;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CQRS.WebApi.Features.ProductFeatures.Queries
+{
+    //suggests that we intend to return an IEnumerable list of Products from this Class implementing
+    //the IRequest interface of MediatR
+    public class GetAllProductsQuery : IRequest<IEnumerable<Product>>
+    {
+        //Every request must have a request handler
+        public class GetAllProductsQueryHandler : IRequestHandler<GetAllProductsQuery, IEnumerable<Product>>
+        {
+            private readonly IApplicationContext _context;
+            public GetAllProductsQueryHandler(IApplicationContext context)
+            {
+                _context = context;
+            }
+
+            // define how the request is being handled. 
+            //This is almost the same for all types of requests and commands
+            public async Task<IEnumerable<Product>> Handle(GetAllProductsQuery query, CancellationToken cancellationToken)
+            {
+                var productList = await _context.Products.ToListAsync();
+                if (productList == null)
+                {
+                    return null;
+                }
+                return productList.AsReadOnly();
+            }
+
+        }
+    }
+}
---
+++ CQRS.WebApi/Features/ProductFeatures/Queries/GetProductByIdQuery.cs
@@ -0,0 +1,32 @@
+﻿using CQRS.WebApi.Context;
+using CQRS.WebApi.Models;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CQRS.WebApi.Features.ProductFeatures.Queries
+{
+    public class GetProductByIdQuery : IRequest<Product>
+    {
+        // is the id of the product we need to fetch.
+        public int Id { get; set; }
+        public class GetProductByIdQueryHandler : IRequestHandler<GetProductByIdQuery, Product>
+        {
+            private readonly IApplicationContext _context;
+            public GetProductByIdQueryHandler(IApplicationContext context)
+            {
+                _context = context;
+            }
+            public async Task<Product> Handle(GetProductByIdQuery query, CancellationToken cancellationToken)
+            {
+                // we query the database and fetch the record with Id as our query Id
+                var product = _context.Products.Where(a => a.Id == query.Id).FirstOrDefault();
+                if (product == null) return null;
+                return product;
+            }
+        }
+    }
+}
